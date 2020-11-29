d347288deae4ed97d0b9a5c32fc4d256d8f984a0
linoandy <linoandy@gmail.com>
2020-11-29 12:21:12 +0800

    finish deviations from normality lab module

--- Portfolio_ Construction_and_Analysis/edhec_risk_kit.py
+++ Portfolio_ Construction_and_Analysis/edhec_risk_kit.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import scipy
 
 def drawdown(return_series: pd.Series):
     """
@@ -30,3 +31,49 @@ def get_ffme_returns():
     rets = rets/100
     rets.index = pd.to_datetime(rets.index, format="%Y%m").to_period('M')
     return rets
+
+
+def get_hfi_returns():
+    """
+    Load and format the EDHEC Hedge Fund Index Returns
+    """
+    hfi = pd.read_csv("data/edhec-hedgefundindices.csv",
+             header=0, index_col=0, parse_dates=True
+            )
+    hfi = hfi/100
+    hfi.index = hfi.index.to_period('M')
+    return hfi
+
+def skewness(r):
+    """
+    Alternative to scipy.stats.skew()
+    Computes the skewness of the supplied Series of the DataFrame
+    Returns a float or a Series
+    """
+    demeaned_r = r - r.mean()
+    # use the population standard deviation, so set dof=0
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r**3).mean()
+    return exp/sigma_r**3
+
+def kurtosis(r):
+    """
+    Alternative to scipy.stats.kurtosis()
+    Computes the kurtosis of the supplied Series of the DataFrame
+    Returns a float or a Series
+    """
+    demeaned_r = r - r.mean()
+    # use the population standard deviation, so set dof=0
+    sigma_r = r.std(ddof=0)
+    exp = (demeaned_r**4).mean()
+    return exp/sigma_r**4
+
+def is_normal(r, level=0.01):
+    """
+    Applies the Jarque-Bera test to dertermine if a Series is normal or not
+    Test is applied at the 1% confidence level by default
+    Returns True if the hypothesis of normality is accepted, False otherwise
+    """
+    statistic, p_value = scipy.stats.jarque_bera(r)
+    return p_value > level
+
