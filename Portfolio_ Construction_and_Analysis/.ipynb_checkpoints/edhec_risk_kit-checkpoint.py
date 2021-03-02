92706fc09320abad74183ed7b02a88080564d96a
linoandy <linoandy@gmail.com>
2021-03-02 23:28:13 +0800

    finish session CIR Model and cash vs ZC bonds

--- Portfolio_ Construction_and_Analysis/.ipynb_checkpoints/edhec_risk_kit-checkpoint.py
+++ Portfolio_ Construction_and_Analysis/.ipynb_checkpoints/edhec_risk_kit-checkpoint.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import scipy
 import scipy.stats
+import math
 from scipy.optimize import minimize
 
 def drawdown(return_series: pd.Series):
@@ -446,3 +447,80 @@ def gbm(n_years = 10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, 
     rets_plus_1[0] = 1
     ret_val = s_0*pd.DataFrame(rets_plus_1).cumprod() if prices else rets_plus_1-1
     return ret_val
+
+def discount(t, r):
+    '''
+    Compute the price of a pure discount bond that pays a dollar at time t, given interest rate r
+    '''
+    return (1+r)**(-t)
+
+def pv(l, r):
+    '''
+    Compute the present value of a sequence of liabilities
+    l is indexed by the time , and the value are the amounts of each liability
+    returns the present value of the sequence
+    '''
+    dates = l.index
+    discounts = discount(dates, r)
+    return (discounts*l).sum()
+
+def funding_ratio(assets, liabilities, r):
+    '''
+    Computes the funding ratio of some assets given liabilities and interest rate
+    '''
+    return assets / pv(liabilities, r)
+
+def inst_to_ann(r):
+    """
+    Convert an instantaneous interest rate to an annual interest rate
+    """
+    return np.expm1(r)
+    # np.expm1(r) is the same as np.exp(r)-1
+
+def ann_to_inst(r):
+    """
+    Convert an instantaneous interest rate to an annual interest rate
+    """
+    return np.log1p(r)
+    # np.log1p(r) is the same as np.log(1+r)
+
+def cir(n_years = 10, n_scenarios=1, a=0.05, b=0.03, sigma=0.05, steps_per_year=12, r_0=None):
+    """
+    Generate random interest rate evolution over time using the CIR model
+    b and r_0 are assumed to be the annualized rates, not the short rate
+    and the returned values are the annualized rates as well
+    """
+    if r_0 is None: r_0 = b 
+    r_0 = ann_to_inst(r_0)
+    dt = 1/steps_per_year
+    num_steps = int(n_years*steps_per_year) + 1 # because n_years might be a float
+    
+    shock = np.random.normal(0, scale=np.sqrt(dt), size=(num_steps, n_scenarios))
+    rates = np.empty_like(shock)
+    rates[0] = r_0
+
+    ## For Price Generation
+    h = math.sqrt(a**2 + 2*sigma**2)
+    prices = np.empty_like(shock)
+    ####
+
+    def price(ttm, r):
+        _A = ((2*h*math.exp((h+a)*ttm/2))/(2*h+(h+a)*(math.exp(h*ttm)-1)))**(2*a*b/sigma**2)
+        _B = (2*(math.exp(h*ttm)-1))/(2*h + (h+a)*(math.exp(h*ttm)-1))
+        _P = _A*np.exp(-_B*r)
+        return _P
+    prices[0] = price(n_years, r_0)
+    ####
+    
+    for step in range(1, num_steps):
+        r_t = rates[step-1]
+        d_r_t = a*(b-r_t)*dt + sigma*np.sqrt(r_t)*shock[step]
+        rates[step] = abs(r_t + d_r_t)
+        # generate prices at time t as well ...
+        prices[step] = price(n_years-step*dt, rates[step])
+
+    rates = pd.DataFrame(data=inst_to_ann(rates), index=range(num_steps))
+    ### for prices
+    prices = pd.DataFrame(data=prices, index=range(num_steps))
+    ###
+    return rates, prices
--- test_drive/rui_risk_kit.py
+++ test_drive/rui_risk_kit.py
@@ -4,6 +4,7 @@ import scipy
 import scipy.stats
 from scipy.optimize import minimize
 import akshare as ak
+import math
 
 def get_mf_values(m_funds, indicator="累计净值走势"):
     '''
@@ -446,4 +447,101 @@ def summary_stats(r, riskfree_rate=0.03):
         "Max Drawdown": dd
     })
 
+def gbm(n_years = 10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, s_0=100.0, prices=True):
+    """
+    Evolution of Geometric Brownian Motion trajectories, such as for Stock Prices through Monte Carlo
+    :param n_years:  The number of years to generate data for
+    :param n_paths: The number of scenarios/trajectories
+    :param mu: Annualized Drift, e.g. Market Return
+    :param sigma: Annualized Volatility
+    :param steps_per_year: granularity of the simulation
+    :param s_0: initial value
+    :return: a numpy array of n_paths columns and n_years*steps_per_year rows
+    """
+    # Derive per-step Model Parameters from User Specifications
+    dt = 1/steps_per_year
+    n_steps = int(n_years*steps_per_year) + 1
+    # the standard way ...
+    # rets_plus_1 = np.random.normal(loc=mu*dt+1, scale=sigma*np.sqrt(dt), size=(n_steps, n_scenarios))
+    # without discretization error ...
+    rets_plus_1 = np.random.normal(loc=(1+mu)**dt, scale=(sigma*np.sqrt(dt)), size=(n_steps, n_scenarios))
+    rets_plus_1[0] = 1
+    ret_val = s_0*pd.DataFrame(rets_plus_1).cumprod() if prices else rets_plus_1-1
+    return ret_val
 
+def discount(t, r):
+    '''
+    Compute the price of a pure discount bond that pays a dollar at time t, given interest rate r
+    '''
+    return (1+r)**(-t)
+
+def pv(l, r):
+    '''
+    Compute the present value of a sequence of liabilities
+    l is indexed by the time , and the value are the amounts of each liability
+    returns the present value of the sequence
+    '''
+    dates = l.index
+    discounts = discount(dates, r)
+    return (discounts*l).sum()
+
+def funding_ratio(assets, liabilities, r):
+    '''
+    Computes the funding ratio of some assets given liabilities and interest rate
+    '''
+    return assets / pv(liabilities, r)
+
+def inst_to_ann(r):
+    """
+    Convert an instantaneous interest rate to an annual interest rate
+    """
+    return np.expm1(r)
+    # np.expm1(r) is the same as np.exp(r)-1
+
+def ann_to_inst(r):
+    """
+    Convert an instantaneous interest rate to an annual interest rate
+    """
+    return np.log1p(r)
+    # np.log1p(r) is the same as np.log(1+r)
+
+def cir(n_years = 10, n_scenarios=1, a=0.05, b=0.03, sigma=0.05, steps_per_year=12, r_0=None):
+    """
+    Generate random interest rate evolution over time using the CIR model
+    b and r_0 are assumed to be the annualized rates, not the short rate
+    and the returned values are the annualized rates as well
+    """
+    if r_0 is None: r_0 = b 
+    r_0 = ann_to_inst(r_0)
+    dt = 1/steps_per_year
+    num_steps = int(n_years*steps_per_year) + 1 # because n_years might be a float
+    
+    shock = np.random.normal(0, scale=np.sqrt(dt), size=(num_steps, n_scenarios))
+    rates = np.empty_like(shock)
+    rates[0] = r_0
+
+    ## For Price Generation
+    h = math.sqrt(a**2 + 2*sigma**2)
+    prices = np.empty_like(shock)
+    ####
+
+    def price(ttm, r):
+        _A = ((2*h*math.exp((h+a)*ttm/2))/(2*h+(h+a)*(math.exp(h*ttm)-1)))**(2*a*b/sigma**2)
+        _B = (2*(math.exp(h*ttm)-1))/(2*h + (h+a)*(math.exp(h*ttm)-1))
+        _P = _A*np.exp(-_B*r)
+        return _P
+    prices[0] = price(n_years, r_0)
+    ####
+    
+    for step in range(1, num_steps):
+        r_t = rates[step-1]
+        d_r_t = a*(b-r_t)*dt + sigma*np.sqrt(r_t)*shock[step]
+        rates[step] = abs(r_t + d_r_t)
+        # generate prices at time t as well ...
+        prices[step] = price(n_years-step*dt, rates[step])
+
+    rates = pd.DataFrame(data=inst_to_ann(rates), index=range(num_steps))
+    ### for prices
+    prices = pd.DataFrame(data=prices, index=range(num_steps))
+    ###
+    return rates, prices
